78c828450823e8e1dcda2a45e0c02a161e283e23
cavaldos <khanhnguyen297kn@gmail.com>
2023-11-03 21:45:57 +0700

    init

--- client/src/pages/dentist/index.jsx
+++
@@ -1,9 +0,0 @@
-const DentistPage = () => {
-  return (
-    <div>
-      <h1 className="text-red-800">Dentist Page</h1>
-    </div>
-  );
-};
-
-export default DentistPage;
--- client/src/pages/guest/index.jsx
+++
@@ -1,9 +0,0 @@
-const GuestPage = () => {
-  return (
-    <div>
-      <h1 className="text-red-800">Guest Page</h1>
-    </div>
-  );
-};
-
-export default GuestPage;
