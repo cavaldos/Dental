0e0167b716af08d18fd2406c17f059bfb2d4b484
cavaldos <khanhnguyen297kn@gmail.com>
2023-11-05 16:10:23 +0700

    loai thuoc

--- SQL/Final.sql
+++ SQL/Final.sql
@@ -942,28 +942,6 @@ GO
 
 -----------------------------------------
 
------------------------------------------
-
-
-
--- =====                           TRIGGER LOAI THUOC
--- R7: Mỗi một loại thuốc trong chi tiết thuốc phải tồn tại trong kho thuốc
--- R10: Số lượng thuốc trong chi tiết thuốc phải lớn hơn 0
--- R11: MATHUOC, STT, SODT trong chi tiết thuốc phải là độc quyền
--- R13: Với mọi chi tiết thuốc,tồn tại số thứ tự và số điện thoại ứng với một hồ sơ bệnh án
--- R16: Với mọi chi tiết dịch vụ
--- tồn tại mã dịch vụ ứng vọi loại dịch vụ đó
--- R3: Số lượng thuốc tồn kho mỗi loại thuốc phải từ 0 trở lên
--- R4: Đơn giá mỗi loại thuốc phải từ 0 trở lên
--- R5: Số lượng thuốc nhập của mỗi loại thuốc phải lớn hơn 0
--- R7: Mỗi một loại thuốc trong chi tiết thuốc phải tồn tại trong kho thuốc
--- R6: Số lượng đã hủy của mỗi loại thuốc phải từ 0 trở lên
--- R8: Ngày hết hạn của mỗi loại thuốc phải xa hơn ngày hiện tại
--- R9: Tổng số lượng thuốc đã nhập phải bằng của tổng số lượng thuốc tồn kho và số lượng thuốc đã hủy và tổng số lượng thuốc trong chi tiết thuốc
--- R14: Mã dịch vụ trong loại dịch vụ phải là độc quyền
--- R15: Đơn giá trong mỗi loại dịch vụ phải lớn hơn 0
--- R15: Với mọi chi tiết dịch vụ
--- tồn tại mã dịch vụ ứng vọi loại dịch vụ đó
                     -- TRIGGER LOAI THUOC
 -- 1. Trigger cập nhật số lượng tồn khi thêm sửa/xóa loại thuốc:
 CREATE TRIGGER Trigger_Insert_Update_Delete_LT on LOAITHUOC for INSERT, UPDATE, DELETE
