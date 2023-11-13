eeb189713c9dda0c68e8c165ca7446a5ecb070ab
cavaldos <khanhnguyen297kn@gmail.com>
2023-11-14 00:39:57 +0700

    proc KH

--- SQL/ProcedureKH.sql
+++ SQL/ProcedureKH.sql
@@ -1,23 +1,20 @@
--- 1 .Xem thông tin cá nhân khách hàng
--- 2. Sửa thông tin cá nhan KH
--- 3. Xem lịch hẹn
--- 4. Xoa lịch hẹn
--- 5. Them lịch hẹn
--- 6. Xem lịch rảnh của nha sĩ (xem trên CA)
--- 7. Xem thông tin nha sĩ
--- 8. Xem hóa đơn
--- 9. Xem hồ sơ bệnh án 
---10. Xem tên nhân viên trong hóa đơn
---11. Xem loại dịch vụ
---12. Xem chi tiết thuốc trong mỗi đơn thuốc
---13. Xem tên các loại thuốc
+-- 1. Tạo tài khoản KH
+-- 2 .Xem thông tin cá nhân khách hàng
+-- 3. Cập nhật thông tin cá nhan KH
+-- 4. Xem lịch hẹn
+-- 5. Xoa lịch hẹn
+-- 6. Them lịch hẹn
+-- 7. Xem lịch rảnh của nha sĩ (xem trên CA)
+-- 8. Truy vấn thuốc
+-- 9. truy vấn loại dịch vụ
+-- 
 
 USE PKNHAKHOA
 GO
 
 --------------
---1/ Tạo tài khoản Khách Hàng (KH, NV)
-CREATE OR ALTER PROC SP_TAOTKKH_NV_KH
+--1/ Tạo tài khoản Khách Hàng (KH)
+CREATE OR ALTER PROC SP_TAOTKKH_KH
 	@SODT VARCHAR(100),
 	@HOTEN NVARCHAR(50),
 	@PHAI NVARCHAR(5),
@@ -51,7 +48,7 @@ BEGIN TRAN
 COMMIT TRAN
 
 --------------
--- 1 .Xem thông tin cá nhân khách hàng
+-- 2 .Xem thông tin cá nhân khách hàng
 CREATE OR ALTER PROC SP_XEMTHONGTIN_KH
     @SODT VARCHAR(100)
 AS
@@ -77,7 +74,7 @@ BEGIN TRAN
 COMMIT TRAN
 
 
--- 2. Sửa thông tin cá nhan KH
+-- 3. Sửa thông tin cá nhan KH
 CREATE OR ALTER PROC SP_SUA_THONGTIN_KH
 @SODT VARCHAR(10),  
 @HOTEN NVARCHAR(50),
@@ -168,43 +165,7 @@ BEGIN TRAN
 COMMIT TRAN
 
 -- 5. Them lịch hẹn
-CREATE OR ALTER PROC SP_THEMLICHHEN_KH
-@SODT VARCHAR(10),
-@MANS VARCHAR(10),  
-@SOTT INT,
-@LYDOKHAM NVARCHAR(200)
-AS
-BEGIN TRAN
-    BEGIN TRY
-        IF EXISTS (SELECT 1     
-               FROM LICHRANH
-               WHERE MANS = @MANS AND SOTT = @SOTT)
-    BEGIN
-      IF EXISTS(SELECT 1 
-                FROM LICHHEN 
-                WHERE MANS = @MANS AND SOTT = @SOTT)
-      BEGIN
-        RAISERROR(N'Lịch hẹn đã tồn tại', 16, 1)
-        ROLLBACK TRAN
-        RETURN  
-      END
-    END
-    ELSE
-    BEGIN
-      RAISERROR(N'Lịch rảnh không tồn tại', 16, 1)
-      ROLLBACK TRAN
-      RETURN 
-    END
-    INSERT INTO LICHHEN(SODT, MANS, SOTT, LYDOKHAM)
-    VALUES(@SODT, @MANS, @SOTT, @LYDOKHAM, @NGAY)
-  END TRY
-  BEGIN CATCH
-    ROLLBACK TRAN
-    DECLARE @errorMessage NVARCHAR(200) = ERROR_MESSAGE();
-    THROW 51000, @errorMessage, 1
-    RETURN 
-  END CATCH
-COMMIT TRAN
+-- ...
 
 -- 6. Xem lịch rảnh của nha sĩ (xem trên CA)
 CREATE OR ALTER PROC SP_XEMLICHRANH_NHASI
@@ -214,8 +175,8 @@ BEGIN TRAN
     BEGIN TRY
         IF EXISTS(SELECT 1 FROM NHASI WHERE MANS = @MANS)
         BEGIN
-            SELECT LR.MANS, LR.SOTT, LR.NGAY, LR.GIO
-            FROM LICHRANH LR
+            SELECT LR.MANS, LR.SOTT, LR.NGAY ,C.MACA, C.GIOBATDAU, C.GIOKETTHUC
+            FROM LICHRANH LR JOIN CA C ON LR.MACA = C.MACA
             WHERE LR.MANS = @MANS
         END
         ELSE
@@ -233,169 +194,88 @@ BEGIN TRAN
     END CATCH
 COMMIT TRAN
 
--- 7. Xem thông tin nha sĩ
-CREATE OR ALTER PROC SP_XEMTHONGTIN_NHASI
-@MANS VARCHAR(10)
-AS  
-BEGIN TRAN 
-    BEGIN TRY
-        IF EXISTS(SELECT 1 FROM NHASI WHERE MANS = @MANS)
-        BEGIN
-            SELECT  NS.HOTEN, NS.PHAI, NS.GIOITHIEU
-            FROM NHASI NS
-            WHERE NS.MANS = @MANS
-        END
-        ELSE
-        BEGIN
-            RAISERROR(N'Tài khoản không tồn tại trong hệ thống', 16, 1);
-            ROLLBACK TRAN
-            RETURN
-        END
-    END TRY
-    BEGIN CATCH
-        ROLLBACK TRAN;
-        DECLARE @errorMessage NVARCHAR(200) = ERROR_MESSAGE();
-        THROW 51000, @errorMessage, 1;
-        RETURN
-    END CATCH
-COMMIT TRAN
 
--- 8. Xem hóa đơn
-CREATE OR ALTER PROC SP_XEMHOADON
-@SODT VARCHAR(10),
-@SOTT INT
-AS
-BEGIN TRAN
-    BEGIN TRY
-        IF EXISTS(SELECT 1 FROM HOADON WHERE SODT = @SODT AND SOTT = @SOTT)
-        BEGIN
-            SELECT HD.SODT, HD.SOTT, HD.MANV, HD.NGAYLAP, HD.TONGTIEN
-            FROM HOADON HD
-            WHERE HD.SODT = @SODT AND HD.SOTT = @SOTT
-        END
-        ELSE
-        BEGIN
-            RAISERROR(N'Hóa đơn không tồn tại trong hệ thống', 16, 1);
-            ROLLBACK TRAN
-            RETURN
-        END
-    END TRY
-    BEGIN CATCH
-        ROLLBACK TRAN;
-        DECLARE @errorMessage NVARCHAR(200) = ERROR_MESSAGE();
-        THROW 51000, @errorMessage, 1;
-        RETURN
-    END CATCH
-COMMIT TRAN
-
--- 9. Xem hồ sơ bệnh án 
-CREATE OR ALTER PROC sp_XemHoSoBenhAn
-@SODT VARCHAR(10),
-@SOTT INT
-AS
-BEGIN TRAN
-    BEGIN TRY
-        IF EXISTS(SELECT 1 FROM HOSOBENH WHERE SODT = @SODT AND SOTT = @SOTT)
-        BEGIN
-            SELECT HSB.SODT, HSB.SOTT, HSB.MANS, HSB.NGAYKHAM, HSB.CHANDOAN, HSB._DAXUATHOADON
-            FROM HOSOBENH HSB
-            WHERE HSB.SODT = @SODT AND HSB.SOTT = @SOTT
-        END
-        ELSE
-        BEGIN
-            RAISERROR(N'Hồ sơ bệnh án không tồn tại trong hệ thống', 16, 1);
-            ROLLBACK TRAN
-            RETURN
-        END
-    END TRY
-    BEGIN CATCH
-        ROLLBACK TRAN;
-        DECLARE @errorMessage NVARCHAR(200) = ERROR_MESSAGE();
-        THROW 51000, @errorMessage, 1;
-        RETURN
-    END CATCH
-COMMIT TRAN
---10. Xem tên nhân viên trong hóa đơn
-CREATE OR ALTER PROC sp_XemTenNVTrongHoaDon
-@SODT VARCHAR(10),
-@SOTT INT
-AS
-BEGIN TRAN
-    BEGIN TRY
-        IF EXISTS(SELECT 1 FROM HOADON WHERE SODT = @SODT AND SOTT = @SOTT)
-        BEGIN
-            SELECT HD.MANV, NV.HOTEN
-            FROM HOADON HD, NHANVIEN NV
-            WHERE HD.MANV = NV.MANV AND HD.SODT = @SODT AND HD.SOTT = @SOTT
-        END
-        ELSE
-        BEGIN
-            RAISERROR(N'Hóa đơn không tồn tại trong hệ thống', 16, 1);
-            ROLLBACK TRAN
-            RETURN
-        END
-    END TRY
-    BEGIN CATCH
-        ROLLBACK TRAN;
-        DECLARE @errorMessage NVARCHAR(200) = ERROR_MESSAGE();
-        THROW 51000, @errorMessage, 1;
-        RETURN
-    END CATCH
-COMMIT TRAN
---11. Xem loại dịch vụ
-CREATE OR ALTER PROC sp_XemLoaiDichVu
-AS
-BEGIN TRAN
-    BEGIN TRY
-        SELECT LDV.MADV, LDV.TENDV, LDV.GIA
-        FROM LOAIDICHVU LDV
-    END TRY
-    BEGIN CATCH
-        ROLLBACK TRAN;
-        DECLARE @errorMessage NVARCHAR(200) = ERROR_MESSAGE();
-        THROW 51000, @errorMessage, 1;
-        RETURN
-    END CATCH
-COMMIT TRAN
+--7  xem lich ranh cua tat ca cac nha si tu hien tai dn 30 ngay sau
+-- khong biet lam
 
 
---12. Xem chi tiết thuốc trong mỗi đơn thuốc
-CREATE OR ALTER PROC sp_XemCTThuocTrongDonThuoc
-@SODT VARCHAR(10),  
-@SOTT INT
+-- 8. truy van loai thuoc
+CREATE OR ALTER PROC SP_TRUYVANTHUOC
+@MA_THUOC VARCHAR(10)
 AS
-BEGIN TRAN 
+BEGIN TRAN
     BEGIN TRY
-        IF EXISTS(SELECT 1 FROM CHITIETTHUOC WHERE SODT = @SODT AND SOTT = @SOTT)
+        IF EXISTS(SELECT 1 FROM CHITIETTHUOC WHERE MATHUOC = @MA_THUOC)
         BEGIN
-            SELECT CTT.MATHUOC, LT.TENTHUOC, CTT.SOLUONG, CTT.CACHDUNG
-            FROM CHITIETTHUOC CTT, LOAITHUOC LT
-            WHERE CTT.MATHUOC = LT.MATHUOC AND CTT.SODT = @SODT AND CTT.SOTT = @SOTT
+            SELECT LT.TENTHUOC,LT.DONVITINH,LT.DONGIA,CTT.SOLUONG
+            FROM CHITIETTHUOC CTT JOIN LOAITHUOC LT ON CTT.MATHUOC = LT.MATHUOC
+            WHERE CTT.MATHUOC = @MA_THUOC
         END
         ELSE
         BEGIN
-            RAISERROR(N'Đơn thuốc không tồn tại trong hệ thống', 16, 1);
+            RAISERROR('Khong tim thay thong tin thuoc',16,1)
             ROLLBACK TRAN
             RETURN
         END
     END TRY
     BEGIN CATCH
-        ROLLBACK TRAN;
+        ROLLBACK TRAN
         DECLARE @errorMessage NVARCHAR(200) = ERROR_MESSAGE();
         THROW 51000, @errorMessage, 1;
         RETURN
     END CATCH
+
 COMMIT TRAN
---13. Xem tên các loại thuốc
-CREATE OR ALTER PROC sp_XemTenCacLoaiThuoc
+
+-- 9. truy van loai dich vu
+CREATE OR ALTER PROC SP_TRUYDICHVU
+@MA_MADV VARCHAR(10)
 AS
 BEGIN TRAN
     BEGIN TRY
-        SELECT LT.MATHUOC, LT.TENTHUOC, LT.DONVITINH, LT.DONGIA
-        FROM LOAITHUOC LT
+        IF EXISTS(SELECT 1 FROM LOAIDICHVU WHERE MADV = @MA_MADV)
+        BEGIN
+            SELECT LDV.MADV, LDV.TENDV, LDV.DONGIA, LDV.MOTA
+            FROM LOAIDICHVU LDV
+            WHERE   LDV.MADV = @MA_MADV
+        END
+        ELSE
+        BEGIN
+            RAISERROR(' Khong ton tai ma dich vu nay', 16, 1);
+            ROLLBACK TRAN
+            RETURN
+        END
     END TRY
     BEGIN CATCH
-        ROLLBACK TRAN;
+        ROLLBACK TRAN
+        DECLARE @errorMessage NVARCHAR(200) = ERROR_MESSAGE();
+        THROW 51000, @errorMessage, 1;
+        RETURN
+    END CATC
+-- 10 -- Truy van lich hen
+CREATE OR ALTER PROC SP_TRUYVANLICHHEN
+    @SDT VARCHAR(10)
+AS
+BEGIN TRAN
+    BEGIN TRY
+        IF EXISTS(SELECT 1 FROM KHACHHANG WHERE SODT = @SDT)
+        BEGIN
+            SELECT NS.MANS , NS.HOTEN, LR.SOTT, C.GIOBATDAU, C.GIOKETTHUC,C.MACA,LH.LYDOKHAM
+            FROM KHACHHANG KH JOIN LICHHEN LH ON KH.SODT = LH.SODT 
+                            JOIN NHASI NS ON LH.MANS = NS.MANS 
+                            JOIN LICHRANH LR ON LH.MANS = LR.MANS 
+                            JOIN CA C ON LR.MACA = C.MACA
+            WHERE KH.SODT = @SDT
+        END
+        ELSE
+        BEGIN
+            RAISERROR(' Khong ton tai khach hang nay', 16, 1);
+            ROLLBACK TRAN
+            RETURN
+        END
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRAN
         DECLARE @errorMessage NVARCHAR(200) = ERROR_MESSAGE();
         THROW 51000, @errorMessage, 1;
         RETURN
