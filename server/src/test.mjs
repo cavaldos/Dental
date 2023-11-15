d1d77d49762b096107be769493f26d337ef1395f
cavaldos <khanhnguyen297kn@gmail.com>
2023-11-15 17:26:56 +0700

    init

--- server/src/test.mjs
+++ server/src/test.mjs
@@ -22,53 +22,7 @@ const data = await load(
 )`,
   role
 );
-// console.log(data);
 
-// const groupedData = _.groupBy(data, "NGAY");
-
-// const results = _.map(groupedData, (items, date) => ({
-//   NGAY: items[0].NGAY,
-//   NS_COHEN: [],
-//   NS_CHUACOHEN: items,
-// }));
-
-// console.log(results);
-
-
-
-// // console.log("data 2",data2);
-
-// const result = {};
-// data.map(item => {    /// data là cái kết quả của hàm load
-//   const ngay = item.NGAY;
-//   if (!result[ngay]) {
-//     result[ngay] = {
-//       NGAY: ngay,
-//       NS_COHEN: [],
-//       NS_CHUACOHEN: []
-//     };
-//   }
-//   if (item.SOTTLH) {
-//     result[ngay].NS_COHEN.push(item);
-//   } else {
-//     result[ngay].NS_CHUACOHEN.push(item);
-//   }
-// });
-// const groupedResult = Object.values(result);
-// console.log(groupedResult);
-
-// console.log("NGAY",groupedResult.map((item) => item.NGAY));
-// console.log("NS COHEN",groupedResult.map((item) => item.NS_COHEN));
-// console.log("NS CHƯA co Hen",groupedResult.map((item) => item.NS_CHUACOHEN));
-
-// Khởi tạo các mảng chứa kết quả
-// const result = {
-//   dichVu: [],
-//   thuoc: []
-// };
-
-// Duyệt qua các phần tử kết quả
-// Khởi tạo mảng kết quả
 const data2 = await load(
   `
 SELECT CA.MACA, GIOBATDAU, GIOKETTHUC, LH.SOTT SOTTLH, LH.MANS, NS.HOTEN HOTENNS, KH.SODT SODTKH, KH.HOTEN HOTENKH, LH.LYDOKHAM LYDOKHAM
@@ -85,12 +39,6 @@ SELECT CA.MACA, GIOBATDAU, GIOKETTHUC, LH.SOTT SOTTLH, LH.MANS, NS.HOTEN HOTENNS
 console.log( JSON.stringify(data2[0]));
 
 
-
-
-// console.log("data2", data2);
-// const result =  groupAndTable(data2, ["SOTT","SODT"], ["MATHUOC", "MADV", "TENDV", "TENTHUOC"]);
-
-// console.log("result", result);
 
 
 export default function test() {
