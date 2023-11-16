31ea04f8733ca59422c84ed2d2293aee87da9311
phivu3125 <phivu3125@gmail.com>
2023-11-16 22:07:02 +0700

    fix trigger

--- SQL/Trigger.sql
+++ SQL/Trigger.sql
@@ -1,104 +1,3 @@
-use PKNHAKHOA
-GO
-
--- Với mọi ca, giờ bắt đầu phải nhỏ hơn giờ kết thúc.
-CREATE TRIGGER TRIGGER_CA_INSERT_UPDATE_1
-ON CA
-AFTER INSERT, UPDATE
-AS
-BEGIN
-    IF EXISTS (SELECT *
-    FROM inserted
-    WHERE GIOBATDAU >= GIOKETTHUC)
-    BEGIN
-        RAISERROR (N'Giờ bắt đầu phải nhỏ hơn giờ kết thúc.', 16, 1)
-        ROLLBACK TRAN
-        RETURN
-    END
-END;
-GO
-
--- Với mọi ca, giờ kết thúc - giờ bắt đầu = 2 tiếng
-CREATE TRIGGER TRIGGER_CA_INSERT_UPDATE_2
-ON CA
-AFTER INSERT, UPDATE
-AS
-BEGIN
-    IF EXISTS (SELECT 1
-    FROM inserted
-    WHERE DATEDIFF(HOUR, inserted.GIOBATDAU, inserted.GIOKETTHUC) <> 2)
-    BEGIN
-        RAISERROR ('Độ dài ca phải là 2 tiếng.', 16, 1)
-        ROLLBACK TRAN
-        RETURN
-    END
-END;
-GO
-
-
--- Các lịch rảnh của một nha sĩ không được trùng nhau (trùng ca và trùng ngày).
-CREATE TRIGGER TRIGGER_LICHRANH_INSERT_UPDATE_2
-ON LICHRANH
-AFTER INSERT, UPDATE
-AS
-BEGIN
-    IF EXISTS (
-        SELECT MANS, NGAY, MACA
-        FROM (SELECT MANS, NGAY, MACA FROM inserted) AS I
-        GROUP BY MANS, NGAY, MACA
-        HAVING COUNT(*) > 1
-    )
-    BEGIN
-        RAISERROR(N'Không thể cập nhật dòng thành một giá trị đã tồn tại.', 16, 1)
-        ROLLBACK TRAN
-        RETURN
-    END
-END;
-GO
-
--- Mỗi ca trong ngày chỉ được tối đa 2 nha sĩ được đăng ký. 
-CREATE TRIGGER TRIGGER_LICHRANH_INSERT_UPDATE_3
-ON LICHRANH
-AFTER INSERT, UPDATE
-AS
-BEGIN
-    IF EXISTS (
-        SELECT MACA, NGAY, COUNT(*) AS SLNS
-        FROM LICHRANH
-        GROUP BY MACA, NGAY
-        HAVING COUNT(*) > 2
-    )
-    BEGIN
-        RAISERROR(N'Không thể đăng ký nhiều hơn 2 Nha sĩ cho mỗi ca trong một ngày.', 16, 1)
-        ROLLBACK TRAN
-        RETURN
-    END
-END;
-GO
-
--- Mỗi lịch rảnh, ca và ngày cần not null.
-CREATE TRIGGER TRIGGER_LICHRANH_INSERT_UPDATE_4
-ON LICHRANH
-AFTER INSERT, UPDATE
-AS
-BEGIN
-    
-    IF EXISTS (
-        SELECT 1
-        FROM inserted AS i
-        LEFT JOIN CA AS c ON i.MACA = c.MACA
-        WHERE c.MACA IS NULL OR i.NGAY IS NULL
-    )
-    BEGIN
-        RAISERROR(N'Mỗi lịch rảnh cần được liên kết với một thông tin ca và ngày không được NULL.', 16, 1)
-        ROLLBACK TRAN
-        RETURN
-    END
-END;
-GO
-
-
-
 -------------------------------------------------------------------------------------------
 -- USE PKNHAKHOA
 -- GO
@@ -143,18 +42,6 @@ BEGIN
         ROLLBACK TRAN
         RETURN
     END
-
-    ELSE
-    BEGIN
-        UPDATE LOAITHUOC
-        SET SLTON = SLTON - (
-            SELECT SOLUONG
-        FROM inserted
-        WHERE MATHUOC = LOAITHUOC.MATHUOC
-        )
-        FROM LOAITHUOC
-            JOIN inserted ON LOAITHUOC.MATHUOC = inserted.MATHUOC
-    END
 END
 GO
 
@@ -189,27 +76,6 @@ BEGIN
         RAISERROR(N'Lỗi không đủ số lượng thuốc tồn kho để bán',16,1)
         ROLLBACK TRAN
         RETURN
-    END
-    
-    ELSE 
-    BEGIN
-        UPDATE LOAITHUOC
-        SET SLTON = SLTON + (
-            SELECT SOLUONG
-        FROM deleted
-        WHERE MATHUOC = LOAITHUOC.MATHUOC
-        )
-        FROM LOAITHUOC
-            JOIN deleted ON LOAITHUOC.MATHUOC = deleted.MATHUOC
-
-        UPDATE LOAITHUOC
-        SET SLTON = SLTON - (
-            SELECT SOLUONG
-        FROM inserted
-        WHERE MATHUOC = LOAITHUOC.MATHUOC
-        )
-        FROM LOAITHUOC
-            JOIN inserted ON LOAITHUOC.MATHUOC = inserted.MATHUOC
     END
 END
 GO
@@ -250,49 +116,25 @@ GO
 
 CREATE TRIGGER Trigger_Insert_LichHen
 ON LICHHEN
-INSTEAD OF INSERT
+AFTER INSERT
 AS
 BEGIN
-
-	IF (not exists (SELECT * FROM LICHRANH LR JOIN inserted i 
-				ON (LR.MANS = i.MANS AND LR.SOTT = i.SOTT)))
+	IF (EXISTS(SELECT * FROM LICHHEN LH, inserted I WHERE I.MANS = LH.MANS AND I.SOTT = LH.SOTT AND I.SODT != LH.SODT))
 	BEGIN
-		RAISERROR(N'Lỗi: Lịch hẹn không khớp với lịch rảnh của nha sĩ.',16,1)
+		RAISERROR(N'Lỗi: Đã có khách hàng đặt lịch hẹn này.',16,1)
 		ROLLBACK TRAN
 		RETURN
 	END
 
-	IF (exists (SELECT * FROM LICHHEN LH JOIN inserted i
-				ON (LH.MANS = i.MANS AND LH.SOTT = i.SOTT)))
-	BEGIN
-		SELECT * FROM LICHHEN LH 
-		RAISERROR(N'Lỗi: Lịch hẹn đã có người đặt.',16,1)
-		ROLLBACK TRAN
-		RETURN
-	END
-	
-	IF (exists (SELECT * FROM 
-			(
-				SELECT SODT AS SODT1, NGAY AS NGAY1, MACA AS MACA1
-				FROM LICHRANH LR1 JOIN inserted i ON (LR1.MANS = i.MANS AND LR1.SOTT = i.SOTT)
-			)   AS T1 INNER JOIN
-			(
-				SELECT SODT AS SODT2, NGAY AS NGAY2, MACA AS MACA2
-				FROM LICHRANH LR2 JOIN LICHHEN LH ON (LR2.MANS = LH.MANS AND LR2.SOTT = LH.SOTT)
-			)   AS T2
-			ON (SODT1 = SODT2 AND NGAY1 = NGAY2 AND MACA1 = MACA2)))
+	IF ((SELECT COUNT(LR.MANS)
+		FROM LICHRANH LR JOIN (SELECT LH.* FROM LICHHEN LH, inserted I WHERE I.SODT = LH.SODT) LH1
+		ON LR.SOTT = LH1.SOTT AND LR.MANS = LH1.MANS
+		GROUP BY MACA) >= 2)
 	BEGIN
 		RAISERROR(N'Lỗi: Các lịch hẹn của cùng một khách hàng không được trùng ca nhau.',16,1)
 		ROLLBACK TRAN
 		RETURN
 	END
-
-	ELSE
-    BEGIN
-        INSERT INTO LICHHEN (MANS, SOTT, LYDOKHAM, SODT)
-        SELECT MANS, SOTT, LYDOKHAM, SODT
-        FROM inserted;
-    END
 END
 GO
 
@@ -307,72 +149,24 @@ GO
 
 CREATE TRIGGER Trigger_Insert_HoaDon
 ON HOADON
-INSTEAD OF insert
+AFTER INSERT
 AS
 BEGIN
-	IF (not exists (SELECT * FROM HOSOBENH HSB JOIN inserted i 
-				ON (HSB.SODT = i.SODT AND HSB.SOTT = i.SOTT)))
-	BEGIN
-		RAISERROR(N'Lỗi: Không tồn tại hồ sơ bệnh tương ứng.',16,1)
-		ROLLBACK TRAN
-		RETURN
-	END
-
-	IF (not exists (SELECT * FROM NHANVIEN NV JOIN inserted i 
-				ON (NV.MANV = i.MANV)))
-	BEGIN
-		RAISERROR(N'Lỗi: Mã nhân viên phụ trách không hợp lệ.',16,1)
-		ROLLBACK TRAN
-		RETURN
-	END
-
-    DECLARE @NgayXuatHD DATE, @NgayKham DATE;
-    SELECT @NgayXuatHD = i.NGAYXUAT
-    FROM inserted i;
-    SELECT @NgayKham = H.NGAYKHAM
-    FROM HOSOBENH H JOIN inserted i ON H.SODT = i.SODT AND H.SOTT = i.SOTT;
-
-    IF @NgayXuatHD < @NgayKham
+    IF EXISTS(SELECT *
+			  FROM HOSOBENH HSB JOIN inserted I
+			  ON HSB.SODT = I.SODT AND HSB.SOTT = I.SOTT
+			  WHERE NGAYXUAT < NGAYKHAM)
 	BEGIN
 		RAISERROR(N'Lỗi: Ngày xuất hóa đơn không hợp lệ.',16,1)
 		ROLLBACK TRAN
 		RETURN
 	END
-
-	ELSE
-    BEGIN
-        INSERT INTO HOADON (SODT, SOTT, NGAYXUAT, MANV)
-        SELECT SODT, SOTT, NGAYXUAT, MANV
-        FROM inserted;
-    END
 END
 GO
 
 -----------------------------------------
 
------------------------------------------
-
-
-
--- =====                           TRIGGER LOAI THUOC
--- R7: Mỗi một loại thuốc trong chi tiết thuốc phải tồn tại trong kho thuốc
--- R10: Số lượng thuốc trong chi tiết thuốc phải lớn hơn 0
--- R11: MATHUOC, STT, SODT trong chi tiết thuốc phải là độc quyền
--- R13: Với mọi chi tiết thuốc,tồn tại số thứ tự và số điện thoại ứng với một hồ sơ bệnh án
--- R16: Với mọi chi tiết dịch vụ
--- tồn tại mã dịch vụ ứng vọi loại dịch vụ đó
--- R3: Số lượng thuốc tồn kho mỗi loại thuốc phải từ 0 trở lên
--- R4: Đơn giá mỗi loại thuốc phải từ 0 trở lên
--- R5: Số lượng thuốc nhập của mỗi loại thuốc phải lớn hơn 0
--- R7: Mỗi một loại thuốc trong chi tiết thuốc phải tồn tại trong kho thuốc
--- R6: Số lượng đã hủy của mỗi loại thuốc phải từ 0 trở lên
--- R8: Ngày hết hạn của mỗi loại thuốc phải xa hơn ngày hiện tại
--- R9: Tổng số lượng thuốc đã nhập phải bằng của tổng số lượng thuốc tồn kho và số lượng thuốc đã hủy và tổng số lượng thuốc trong chi tiết thuốc
--- R14: Mã dịch vụ trong loại dịch vụ phải là độc quyền
--- R15: Đơn giá trong mỗi loại dịch vụ phải lớn hơn 0
--- R15: Với mọi chi tiết dịch vụ
--- tồn tại mã dịch vụ ứng vọi loại dịch vụ đó
-                    -- TRIGGER LOAI THUOC
+-- TRIGGER LOAI THUOC
 -- 1. Trigger cập nhật số lượng tồn khi thêm sửa/xóa loại thuốc:
 CREATE TRIGGER Trigger_Insert_Update_Delete_LT on LOAITHUOC for INSERT, UPDATE, DELETE
 AS
@@ -406,29 +200,41 @@ BEGIN
         ROLLBACK TRAN
         RETURN
     END
+    
 END
 GO
 
-
--- 2. Trigger cập nhật ngày hết hạn khi thêm/sửa loại thuốc:
-CREATE TRIGGER Trigger_Insert_Update_LT_Hethan on LOAITHUOC for INSERT, UPDATE
+----
+CREATE TRIGGER Trigger_Insert_Update_LT_Hethan 
+ON LOAITHUOC
+INSTEAD OF INSERT
 AS
 BEGIN
--- R8: Ngày hết hạn của mỗi loại thuốc phải xa hơn ngày hiện tại.--
-    IF EXISTS (SELECT * FROM LOAITHUOC LT JOIN inserted I ON LT.MATHUOC = I.MATHUOC WHERE LT.NGAYHETHAN < GETDATE())
-    BEGIN
-        RAISERROR(N'Ngày hết hạn không được nhỏ hơn ngày hiện tại', 16, 1)
-        ROLLBACK TRAN
-        RETURN
-    END
+
+  -- R8: Ngày hết hạn của mỗi loại thuốc phải xa hơn ngày hiện tại.
+  IF EXISTS (
+    SELECT * 
+    FROM LOAITHUOC LT JOIN inserted  I ON LT.MATHUOC = I.MATHUOC 
+    WHERE LT.NGAYHETHAN < GETDATE()
+  )
+  BEGIN
+     RAISERROR(N'Ngày hết hạn không được nhỏ hơn ngày hiện tại', 16, 1)
+     ROLLBACK TRAN
+     RETURN
+  END
+
+  -- Cho phép insert 
+  INSERT INTO LOAITHUOC (MATHUOC, TENTHUOC, DONVITINH, CHIDINH, 
+                          SLTON, SLNHAP, SLDAHUY, NGAYHETHAN, DONGIA)
+  SELECT MATHUOC, TENTHUOC, DONVITINH, CHIDINH,
+         SLTON, SLNHAP, SLDAHUY, NGAYHETHAN, DONGIA
+  FROM inserted
 
 END
-	
-
 
 GO
 
---                                 TRIGGER LOAI DICH VU
+--TRIGGER LOAI DICH VU
 --1.Trigger gia dich vu lon hon 0:
 CREATE TRIGGER Trigger_Insert_Update_LDV on LOAIDICHVU for INSERT, UPDATE
 AS
@@ -455,9 +261,4 @@ BEGIN
         RETURN
     END
 END
-
-
-
-
-
-
+GO
