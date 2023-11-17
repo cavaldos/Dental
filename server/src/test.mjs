04017c4f3ad51e7e578ec00d3027da8eeca32fbc
cavaldos <khanhnguyen297kn@gmail.com>
2023-11-17 22:05:57 +0700

    <html> <head><title>301 Moved Permanently</title></head> <body> <center><h1>301 Moved Permanently</h1></center> </body> </html>

--- server/src/test.mjs
+++ server/src/test.mjs
@@ -40,18 +40,91 @@ SELECT CA.MACA, GIOBATDAU, GIOKETTHUC, LH.SOTT SOTTLH, LH.MANS, NS.HOTEN HOTENNS
 
 
 import fs from "fs";
-const data3 = await load(`SELECT * FROM LOAIDICHVU`, role);
+const data3 = await load(
+  `SELECT  LR.SOTT , LR.MACA,LR.NGAY, C.GIOBATDAU,C.GIOKETTHUC FROM  LICHRANH LR JOIN CA C ON (LR.MACA = C.MACA) JOIN NHASI NS ON (NS.MANS =LR.MANS) `,
+  role
+);
+const data4 = groupAndTable(data3, ["NGAY"], ["MACA"]);
 
 // console.log(data3);
-const jsonData = JSON.stringify(data3, null, 2);
-//Lưu vào file data.json
-fs.writeFile("dv.json", jsonData, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Data written to file");
-  }
-});
+
+
+
+
+
+
+
+
+const result = _.chain(data3)
+  .groupBy("NGAY")
+  .map((values, key) => {
+    return {
+      NGAY: key,
+      CA: _.map(values, (item) => {
+        return {
+          GIOBATDAU: item.GIOBATDAU,
+          GIOKETTHUC: item.GIOKETTHUC,
+        };
+      }),
+      MANS: "NS0001", // giả sử đều NS0001
+      SOTT: values.length, // đếm số lượng phần tử trong ngày
+    };
+  })
+  .value();
+  console.log(result );
+
+  console.log(result.map((item) => item.CA));
+
+
+
+
+
+
+
+
+
+
+const tests = [
+  {
+    NGAY: "2024-01-02T00:00:00.000Z",
+    CA: [
+      { GIOBATDAU: "1970-01-01T19:00:00.000Z" },
+      { GIOKETTHUC: " 1970-01-01T21:00:00.000Z" },
+    ],
+    MANS: "NS0001",
+    SOTT: 3,
+  },
+  {
+    NGAY: "2024-01-02T00:00:00.000Z",
+    CA: [
+      { GIOBATDAU: "1970-01-01T19:00:00.000Z" },
+      { GIOKETTHUC: " 1970-01-01T21:00:00.000Z" },
+    ],
+    MANS: "NS0002",
+    SOTT: 2,
+  },
+];
+
+
+
+
+
+// const data5 = data4.map((obj) => {
+//   const uniqueMACA = _.uniq(obj.MACA);
+//   return { ...obj, MACA: uniqueMACA };
+// });
+
+// console.log(data5);
+
+// const jsonData = JSON.stringify(data5, null, 2);
+// // //Lưu vào file data.json
+// fs.writeFile("lichranh.json", jsonData, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Data written to file");
+//   }
+// });
 
 
 
