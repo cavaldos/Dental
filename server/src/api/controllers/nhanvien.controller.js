98b8f10c04a6febc7999fff4726977d1d3b798d3
vtcong21 <vtcong21@vp.fitus.edu.vn>
2023-11-25 12:05:48 +0700

    done ctrller + route nv

--- server/src/api/controllers/nhanvien.controller.js
+++ server/src/api/controllers/nhanvien.controller.js
@@ -16,7 +16,76 @@ const nhanVienController = {
       return res.status(500).json({ error: 'An error occurred while processing the request' });
     }
   },
-  
+  taoTaiKhoanKH: async (req, res) => {
+    try {
+      if (!pool) {
+        return res.status(500).json({ error: 'Khong the ket noi db' });
+      }
+      const params = req.body;
+      const sp = 'SP_TAOTKKH_NV';
+      const result = await pool.executeSP(sp, params);
+      return res.status(201).json({ success: true });
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+      return res.status(500).json({ error: 'An error occurred while processing the request' });
+    }
+  },
+  taoHoaDon: async (req, res) => {
+    try {
+      if (!pool) {
+        return res.status(500).json({ error: 'Khong the ket noi db' });
+      }
+      const params = req.body;
+      const sp = 'SP_TAOHOADON_NV';
+      const result = await pool.executeSP(sp, params);
+      return res.status(201).json(result);
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+      return res.status(500).json({ error: 'An error occurred while processing the request' });
+    }
+  },
+  xacNhanThanhToan: async (req, res) => {
+    try {
+      if (!pool) {
+        return res.status(500).json({ error: 'Khong the ket noi db' });
+      }
+      const params = req.body;
+      const sp = 'SP_THANHTOANHOADON_NV';
+      const result = await pool.executeSP(sp, params);
+      return res.status(200).json({ success: true });
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+      return res.status(500).json({ error: 'An error occurred while processing the request' });
+    }
+  },
+  getHoaDon: async (req, res) => {
+    try {
+      if (!pool) {
+        return res.status(500).json({ error: 'Khong the ket noi db' });
+      }
+      const params = req.params;
+      const sp = 'SP_GETHOADON_NV';
+      const result = await pool.executeSP(sp, params);
+      return res.status(200).json(result);
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+      return res.status(500).json({ error: 'An error occurred while processing the request' });
+    }
+  },
+  doiMatKhau: async (req, res) => {
+    try {
+      if (!pool) {
+        return res.status(500).json({ error: 'Khong the ket noi db' });
+      }
+      const params = req.body;
+      const sp = 'SP_DOIMATKHAU_NV';
+      const result = await pool.executeSP(sp, params);
+      return res.status(201).json({ success: true });
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+      return res.status(500).json({ error: 'An error occurred while processing the request' });
+    }
+  },
 
 
 };
--- server/src/api/routes/nhanvien.route.js
+++ server/src/api/routes/nhanvien.route.js
@@ -4,4 +4,9 @@ import NhanVienController from "../controllers/nhanvien.controller.js";
 const router = express.Router();
 
 router.get("/lichRanhNS", NhanVienController.getLichRanhNS);
+router.post("/khachHang", NhanVienController.taoTaiKhoanKH);
+router.post("/hoaDon", NhanVienController.taoHoaDon);
+router.put("/xacNhanHoaDon", NhanVienController.xacNhanThanhToan);
+router.puf("/matKhau", NhanVienController.doiMatKhau);
+router.get("/hoaDon/:sdt", NhanVienController.getHoaDon);
 export default router;
