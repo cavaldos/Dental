c7751979bf56961a7fb0c3674c1d1cc626e833c4
phivu3125 <phivu3125@gmail.com>
2023-11-27 19:19:53 +0700

    save

--- server/src/api/controllers/nhanvien.controller.js
+++ server/src/api/controllers/nhanvien.controller.js
@@ -68,10 +68,10 @@ const nhanVienController = {
         return res.status(500).json({ error: 'Khong the ket noi db' });
       }
       const params = {};
-      params.SDT = req.params.sdt;
-      const sp = 'SP_GETHOADON_NV';
+      params.SODT = req.params.sdt;
+      const sp = 'SP_GETHOADON1KH_NV';
       const result = await pool.executeSP(sp, params);
-      return res.status(200).json(result);
+      return res.status(200).json(result[0]);
     } catch (error) {
       console.error('An error occurred:', error.message);
       return res.status(500).json({ error: 'An error occurred while processing the request' });
