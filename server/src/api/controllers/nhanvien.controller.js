186c366afcaef25a641826e1ca79f8657c106f22
phivu3125 <phivu3125@gmail.com>
2023-11-29 21:06:05 +0700

    add groupData

--- server/src/api/controllers/nhanvien.controller.js
+++ server/src/api/controllers/nhanvien.controller.js
@@ -1,5 +1,6 @@
 import { poolConnect } from "../../config/db.mjs";
 const pool = await poolConnect('NV');
+import groupHD from "../../utils/groupData.js"
 
 const nhanVienController = {
   getLichRanhNS: async (req, res) => {
@@ -25,7 +26,13 @@ const nhanVienController = {
       if (!pool) {
         return res.status(500).json({ error: 'Khong the ket noi db' });
       }
-      const params = req.body;
+      const params = {};
+      params.SODT = req.body.sdt;
+      params.HOTEN = req.body.hoten;
+      params.PHAI = req.body.phai;
+      params.NGAYSINH = req.body.ngaysinh;
+      params.DIACHI = req.body.diachi;
+
       const sp = 'SP_TAOTKKH_NV';
       const result = await pool.executeSP(sp, params);
       return res.status(201).json({ success: true });
@@ -34,15 +41,23 @@ const nhanVienController = {
       return res.status(500).json({ error: 'An error occurred while processing the request' });
     }
   },
+  
   taoHoaDon: async (req, res) => {
     try {
       if (!pool) {
         return res.status(500).json({ error: 'Khong the ket noi db' });
       }
-      const params = req.body;
+      const params = {};
+      params.SODT  = req.body.sdt;
+      params.SOTT  = req.body.stt;
+      params.MANV   = req.body.manv;
+
       const sp = 'SP_TAOHOADON_NV';
       const result = await pool.executeSP(sp, params);
-      return res.status(201).json(result);
+      const groupedResult = groupHD(result[0]);
+
+      console.log(groupedResult);
+      return res.status(201).json(groupedResult[0]);
     } catch (error) {
       console.error('An error occurred:', error.message);
       return res.status(500).json({ error: 'An error occurred while processing the request' });
