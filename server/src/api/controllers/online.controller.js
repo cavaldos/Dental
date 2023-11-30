f77bbe00e3d199717d25fa6c238035a5324d0c2e
phivu3125 <phivu3125@gmail.com>
2023-11-30 16:29:44 +0700

    add KH online API

--- server/src/api/controllers/online.controller.js
+++ server/src/api/controllers/online.controller.js
@@ -23,6 +23,51 @@ const onlineController = {
       return res.status(500).json({ error: 'An error occurred while processing the request' });
     }
   },
+  dangnhap: async (req, res) => {
+    try {
+      if (!pool) {
+        return res.status(500).json({ error: 'Khong the ket noi db' });
+      }
+      const params = {};
+      params.MATK = req.body.matk;
+      params.MATKHAU = req.body.matkhau;
+      
 
+      const sp = 'SP_DANGNHAP_ALL';
+      const result = await pool.executeSP(sp, params);
+      return res.status(200).json({ success: true });
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+      return res.status(500).json({ error: 'An error occurred while processing the request' });
+    }
+  },
+  getAllDV: async (req, res) => {
+    try {
+      if (!pool) {
+        return res.status(500).json({ error: 'Khong the ket noi db' });
+      }
+      const params = null;
+      const sp = 'SP_XEMDANHSACHDICHVU_ALL';
+      const result = await pool.executeSP(sp, params);
+      return res.status(200).json(result[0]);
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+      return res.status(500).json({ error: 'An error occurred while processing the request' });
+    }
+  },
+  getAllDSNS: async (req, res) => {
+    try {
+      if (!pool) {
+        return res.status(500).json({ error: 'Khong the ket noi db' });
+      }
+      const params = null;
+      const sp = 'SP_XEMDANHSACHNHASI_ALL';
+      const result = await pool.executeSP(sp, params);
+      return res.status(200).json(result[0]);
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+      return res.status(500).json({ error: 'An error occurred while processing the request' });
+    }
+  },
 };
 export default onlineController;
--- server/src/api/routes/online.route.js
+++ server/src/api/routes/online.route.js
@@ -3,6 +3,10 @@ import OnlineController from "../controllers/online.controller.js";
 
 const router = express.Router();
 router.post("/taoKH", OnlineController.taoTKKH);
+router.get("/dangnhap", OnlineController.dangnhap);
+router.get("/getAllDV", OnlineController.getAllDV);
+router.get("/getAllDSNhaSi", OnlineController.getAllDSNS);
+
 
 
 export default router;
