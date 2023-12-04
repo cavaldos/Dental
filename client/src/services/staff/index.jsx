dbc3d32e3e8bff08ecca9e23878085d83b534558
cavaldos <khanhnguyen297kn@gmail.com>
2023-12-04 16:18:55 +0700

    fix api , add services

--- client/src/services/staff/index.jsx
+++ client/src/services/staff/index.jsx
@@ -1,16 +1,5 @@
 import Axios from "../Axios";
-// router.get("/lichRanhNS", nhanVienController.getLichRanhNS);
-// router.post("/khachHang", nhanVienController.taoTaiKhoanKH);
-// router.post("/hoaDon", nhanVienController.taoHoaDon);
-// router.put("/xacNhanHoaDon", nhanVienController.xacNhanThanhToan);
-// router.put("/matKhau", nhanVienController.doiMatKhau);
-// router.get("/getAllThuoc", nhanVienController.getAllThuoc);
-// router.get("/getAllDV", nhanVienController.getAllDV);
-// router.get("/getAllCa", nhanVienController.getAllCa);
-// router.get("/hoaDon/:sdt", nhanVienController.getHoaDon);
-// router.get("/benhAn/:sdt", nhanVienController.xemBenhAn);
-// router.get("/getAllDSNhaSi", nhanVienController.getAllDSNS);
-// router.delete("/lichHen", nhanVienController.deleteLichHen);
+
 
 
 const StaffService = {
