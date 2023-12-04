60ccba8c2c3d4439316d97ca735268f154f81567
cavaldos <khanhnguyen297kn@gmail.com>
2023-12-04 16:16:51 +0700

    fix api , add services

--- client/src/services/axios.config.jsx
+++ client/src/services/axios.config.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const instance = axios.create({
-  baseURL: "http://192.168.1.53:3000", // import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   timeout: 5000,
   headers: { "X-Custom-Header": "foobar" },
 });
