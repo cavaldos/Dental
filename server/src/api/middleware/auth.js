08be55386518137bb65122f11ddc3662ab739474
vtcong21 <vtcong21@vp.fitus.edu.vn>
2023-12-13 00:41:01 +0700

    check id each auth mdw

--- server/src/api/middleware/auth.js
+++ server/src/api/middleware/auth.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
-
+import { poolConnect } from '../../config/db.mjs';
+const pool = await poolConnect('KHONLINE');
 const authMiddleware = {
     authenticateToken: async (req, res, next) => {
         // Kiểm tra xem header "Authorization" có tồn tại không
@@ -16,6 +17,18 @@ const authMiddleware = {
             const decodedToken = jwt.verify(accessToken, secret);
             if (!decodedToken.userId || !decodedToken.userRole) {
                 return res.status(401).send('Unauthorized!');
+            }
+            try{
+                const params = {
+                    MATK: decodedToken.userId
+                }
+                const result = await pool.executeSP('SP_KTTL_ALL', params);
+                if(result[0][0].ROLE != decodedToken.userRole){
+                    return res.status(400).send({error: 'Wrong ROLE'});
+                }
+                
+            }catch(error){
+                return res.status(401).sned({error: 'Wrong ID or the account is blocked'});
             }
             req.userId = decodedToken.userId;
             req.userRole = decodedToken.userRole;
