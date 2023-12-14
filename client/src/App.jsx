db85d04bb208f183c49a8838e7691df748d86959
cavaldos <khanhnguyen297kn@gmail.com>
2023-12-14 17:21:36 +0700

    fix login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,51 +7,17 @@ import {
 } from "~/routes";
 import React, { Fragment, Suspense, lazy } from "react";
 
-import { useSelector, useDispatch } from "react-redux";
-import { BrowserRouter as Router, Route, Routes,useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Loading from "./components/err/loading";
 const NotfoundError = lazy(() => import("~/components/err"));
-import OnlineService from "./services/online";
-import Axios from "./services/axios.config";
-import GetCookie from "./hooks/GetCookie";
-import {
-  setRole,
-  updateUserInfo,
-  deleteRole,
-} from "~/redux/features/userSlice";
 
+import Auth from "./components/Auth";
 import Test from "./test";
 
 function App() {
   const user = useSelector((state) => state.user);
-  console.log(user);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const pass = GetCookie("password");
-  React.useEffect(() => {
-    OnlineService.checkLogin()
-      .then((res) => {
-        if (res.message === "ok") {
-          Axios.post("/online/dangnhap", {
-            matk: user.MAQTV || user.MANS || user.MANV || user.SODT,
-            matkhau: pass,
-          })
-            .then((resp) => {
-              dispatch(setRole(resp.data.info.ROLE));
-              dispatch(updateUserInfo(resp.data.info));
-            })
-            .catch((error) => {
-              console.log("err", error);
-            });
-        }
-      })
-      .catch((err) => {
-        dispatch(deleteRole());
-        navigate("/signins");
 
-        console.log(err);
-      });
-  }, []);
   const VerifyRoure = () => {
     switch (user.ROLE) {
       case "admin":
@@ -70,6 +36,7 @@ function App() {
   return (
     <>
       <Test />
+      <Auth />
       <Router>
         <Suspense fallback={<Loading />}>
           <Routes>
