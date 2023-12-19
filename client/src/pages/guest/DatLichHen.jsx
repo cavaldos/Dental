4b5d36c44cdce3c99bd9a3580cec48e66c22d59d
cavaldos <khanhnguyen297kn@gmail.com>
2023-12-19 08:17:23 +0700

    fix dat lich khach hang

--- client/src/pages/guest/DatLichHen.jsx
+++ client/src/pages/guest/DatLichHen.jsx
@@ -4,21 +4,30 @@ import { Input } from "antd";
 const { TextArea } = Input;
 import GuestService from "../../services/guest";
 import { useDispatch, useSelector } from "react-redux";
-import { booking } from "../../redux/features/orderSlice";
+import { booking, deleteOder } from "../../redux/features/orderSlice";
+import Buttons from "../../components/button";
+import { ButtonBlue } from "../../components/button";
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+
 const NhaSi = ({ TENNS, MAND }) => {
   const dispath = useDispatch();
   const handleOnClick = () => {
-    dispath(booking({ mans: MAND }));
+    dispath(booking({ mans: MAND, tenns: TENNS }));
     message.success(`Đã chọn nha sĩ ${TENNS}`);
   };
   return (
     <>
-      <Button
+      <button
+        style={{
+          transition: "all 0.4s ",
+        }}
         onClick={() => handleOnClick()}
-        className="p-4 rounded-md border border-slate-400 h-16"
+        className="p-4 border hover:border-[#86b6f8] hover:text-[17px] border-slate-400 h-16 min-w-[220px] rounded-sm hover:bg-slate-200  "
       >
         <h1>{TENNS}</h1>
-      </Button>
+      </button>
     </>
   );
 };
@@ -26,20 +35,35 @@ const NhaSi = ({ TENNS, MAND }) => {
 const Ca = ({ GIOBD, GIOKT, NGAY, SOTT }) => {
   const dispath = useDispatch();
   const handleOnClick = (sott) => {
-    dispath(booking({ sott: sott }));
+    dispath(
+      booking({ sott: sott, GIOBATDAU: GIOBD, GIOKETTHUC: GIOKT, NGAY: NGAY })
+    );
+    message.success(`Đã chọn ca bắt đầu lúc ${GIOBD} ngày ${NGAY}`, 4);
   };
 
   return (
     <>
-      <Button
+      <button
         onClick={() => handleOnClick(SOTT)}
-        className="p-4 rounded-lg border border-slate-400 h-16"
+        className="p-2 border
+         border-slate-400 min-h-16
+          min-w-[20px] rounded-sm  
+         hover:bg-slate-200 
+         hover:border-[#86b6f8]
+            "
       >
-        <h1>{NGAY}</h1>
-        <h1>
-          {GIOBD} - {GIOKT}
-        </h1>
-      </Button>
+        <div className="flex flex-col text-gray-400">
+          <div className="flex gap-3 mb-3">
+            Ngày : <h1 className="ml-auto text-black">{NGAY}</h1>
+          </div>
+          <div className="flex gap-3 ">
+            Bắt đầu : <h1 className="ml-auto text-black">{GIOBD}</h1>
+          </div>{" "}
+          <div className="flex gap-3 ">
+            Kết thúc : <h1 className="ml-auto text-black">{GIOKT}</h1>
+          </div>
+        </div>
+      </button>
     </>
   );
 };
@@ -70,9 +94,16 @@ const ChonCa = () => {
   const [lichRanh, setLichRanh] = useState([]);
   function formatTime(inputDate) {
     const date = new Date(inputDate);
-    const hours = date.getUTCHours().toString().padStart(2, '0'); // Lấy giờ theo múi giờ UTC
-    const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+    const hours = date.getUTCHours().toString().padStart(2, "0"); // Lấy giờ theo múi giờ UTC
+    const minutes = date.getUTCMinutes().toString().padStart(2, "0");
     return `${hours}:${minutes}`;
+  }
+  function formatDate(inputDate) {
+    const date = new Date(inputDate);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
   }
 
   useEffect(() => {
@@ -107,15 +138,13 @@ const ChonCa = () => {
           return item.MANS === order.mans;
         });
         const new_lichRanhformat = new_lichRanh.map((item) => {
-          console.log("func", item)
           return {
             ...item,
-            NGAY: new Date(item.NGAY).toLocaleDateString(),
+            NGAY: formatDate(new Date(item.NGAY)),
             GIOBATDAU: formatTime(new Date(item.GIOBATDAU)),
             GIOKETTHUC: formatTime(new Date(item.GIOKETTHUC)),
           };
         });
-        console.log("lichRanh:", new_lichRanhformat)
         setLichRanh(new_lichRanhformat);
       });
     }
@@ -143,30 +172,45 @@ const ChonCa = () => {
 
 const LyDoKham = () => {
   const [lydokham, setLyDoKham] = useState("");
-  console.log(lydokham);
+  const [isLoading, setIsLoading] = useState(false);
   const user = useSelector((state) => state.user);
-  const dispath = useDispatch();
-  const handleOnClick = () => {
-    dispath(booking({ lydokham: lydokham, sodt: user.SODT }));
-    message.success("Đã chọn lý do khám");
-  };
+  const dispatch = useDispatch();
+  const timeoutRef = React.useRef(null);
+
+  useEffect(() => {
+    clearTimeout(timeoutRef.current);
+    if (lydokham !== "") {
+      setIsLoading(true);
+
+      timeoutRef.current = setTimeout(() => {
+        dispatch(booking({ lydokham: lydokham, sodt: user.SODT }));
+        setIsLoading(false);
+      }, 2000);
+    }
+  }, [lydokham, dispatch, user.SODT]);
 
   return (
     <>
+      <div className=" flex justify-center mb-3">
+        <h1 className=" ">Xin vui lòng nhập lý do khám</h1>
+        {isLoading ? (
+          <Spin
+            indicator={
+              <LoadingOutlined className="text-sm ml-3 text-gray-600" spin />
+            }
+          />
+        ) : (
+          <></>
+        )}
+      </div>
       <div className="flex justify-center">
-        <div className=" w-[60%]">
+        <div className="w-[60%]">
           <TextArea
-            className=" w-full "
+            className="w-full"
             rows={4}
             value={lydokham}
             onChange={(e) => setLyDoKham(e.target.value)}
           />
-          <Button
-            onClick={() => handleOnClick()}
-            className="p-4 rounded-lg border border-slate-400 h-16"
-          >
-            <h1>Xác nhận</h1>
-          </Button>
         </div>
       </div>
     </>
@@ -178,12 +222,33 @@ const XacNhan = () => {
   return (
     <>
       <div class="flex justify-center flex-col text-neutral-900">
-        <div class="mx-auto">
-          <h1 class="text-2xl font-bold">Thong tin dat lich</h1>
-          <h1 class="text-lg font-medium">sdt: {order.sodt}</h1>
-          <h1 class="text-lg font-medium">mans: {order.mans}</h1>
-          <h1 class="text-lg font-medium">sott:{order.sott}</h1>
-          <h1 class="text-lg font-medium">lydokham :{order.lydokham}</h1>
+        <div class="mx-auto bg-slate-500 w-[900px] p-4">
+          <h1 class="text-2xl font-bold mb-5">Đây là thông tin của bạn:</h1>
+
+          <h1 class="text-lg font-medium">
+            Số điện thoại của bạn: {order.sodt}
+          </h1>
+          <div className="flex">
+            <h1 class="text-lg font-medium">Tên nha sĩ: </h1>
+            <h1 class="text-lg font-medium">{order.tenns} </h1>
+          </div>
+
+          <div className="flex">
+            <h1 class="text-lg font-medium">Ngày khám: </h1>
+            <h1 class="text-lg font-medium"> {order.CA.NGAY}</h1>
+          </div>
+          <div className="flex">
+            <h1 class="text-lg font-medium">Bắt đầu: </h1>
+            <h1 class="text-lg font-medium"> {order.CA.GIOBATDAU}</h1>
+          </div>
+          <div className="flex">
+            <h1 class="text-lg font-medium">Kết thúc: </h1>
+            <h1 class="text-lg font-medium"> {order.CA.GIOKETTHUC}</h1>
+          </div>
+          <div className="flex">
+            <h1 class="text-lg font-medium">Lý do khám: </h1>
+            <h1 class="text-lg font-medium"> {order.lydokham}</h1>
+          </div>
         </div>
       </div>
     </>
@@ -210,6 +275,8 @@ const steps = [
 
 const DatLichContainer = () => {
   const [current, setCurrent] = useState(0);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const next = () => {
     setCurrent(current + 1);
   };
@@ -236,46 +303,48 @@ const DatLichContainer = () => {
       message.error("Vui lòng nhập lý do khám");
       return;
     }
-    
+
     await GuestService.taoLichHen(order).then((res) => {
-      console.log(res);
       message.success("Đặt lịch thành công");
+      setTimeout(() => {
+        dispatch(deleteOder());
+      }, 500);
+      setTimeout(() => {
+        navigate("/xem-lich-hen");
+      }, 500);
     });
   };
 
   return (
-    <>
+    <div className=" ">
       <Steps current={current} items={items} />
-      <div className=" min-h-[300px] bg-[#FEFFFE] mt-4 p-4 rounded-lg border">
+      <div className=" min-h-[300px] bg-[#FEFFFE] mt-4 p-4 rounded-lg border max-h-[500px] overflow-y-auto">
         {steps[current].content}
       </div>
       <div className="flex justify-center mt-6">
         {current > 0 && (
-          <Button
-            style={{
-              margin: "0 8px",
-            }}
+          <Buttons
+            className="mr-2 border-dashed border-2 border-blue-500 hover:bg-gray-400 hover:border-solid bg-slate-300 "
             onClick={() => prev()}
-          >
-            Previous
-          </Button>
+            text="Truớc"
+          />
         )}
         {current < steps.length - 1 && (
-          <Button className="bg-blue-500" type="primary" onClick={() => next()}>
-            Next
-          </Button>
+          <ButtonBlue
+            className="bg-blue-500 "
+            func={() => next()}
+            text="Tiếp tục"
+          />
         )}
         {current === steps.length - 1 && (
-          <Button
-            className="bg-green-600 ml-2"
-            type="primary"
+          <Buttons
+            className="bg-green-600 hover:bg-green-700 ml-2"
+            text="Đặt lịch"
             onClick={() => handleBooking()}
-          >
-            Done
-          </Button>
+          />
         )}
       </div>
-    </>
+    </div>
   );
 };
 
@@ -283,7 +352,7 @@ const DatLichHen = () => {
   return (
     <>
       <div className="">
-        <h1 className="mx-auto mb-5">Đặt lịch hẹn</h1>
+        <h1 className="mx-auto mb-5 text-2xl">Đặt lịch hẹn</h1>
         <DatLichContainer />
       </div>
     </>
