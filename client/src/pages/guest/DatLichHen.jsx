1174a31395fb336a93af02c9531f958cbe4108ed
cavaldos <khanhnguyen297kn@gmail.com>
2023-12-20 12:16:38 +0700

    fix

--- client/src/pages/guest/DatLichHen.jsx
+++ client/src/pages/guest/DatLichHen.jsx
@@ -115,23 +115,7 @@ const ChonCa = () => {
   useEffect(() => {
     if (order.mans === "") {
       GuestService.xemLRChuaDatTatCaNSTheoNgay().then((res) => {
-        const newres=res.map((item) => {
-           return {
-            ...item,
-            code: item.MACA + item.NGAY,
-           }
-
-        });
-        console.log(newres);
-        let uniqueObjects = [];
-        newres.forEach((item) => {
-          if (!uniqueObjects.some((uniqueItem) => uniqueItem.code === item.code)) {
-            uniqueObjects.push(item);
-          }
-        }
-        );
-
-        setLichRanh(uniqueObjects);
+        setLichRanh(res);
       });
     } else {
       GuestService.lichRanh().then((res) => {
