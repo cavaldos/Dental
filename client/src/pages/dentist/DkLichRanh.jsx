bd8c0d0f7b294399ae594899ccc5a13c08938cb9
phivu3125 <phivu3125@gmail.com>
2024-01-02 11:48:43 +0700

    save

--- client/src/pages/dentist/DkLichRanh.jsx
+++ client/src/pages/dentist/DkLichRanh.jsx
@@ -1,14 +1,23 @@
 import { Button, Calendar, Select } from "antd";
 import { lichhen4 } from "~/fakedata/lhnv";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TableLichHen from "~/components/dentist/TableLichHen";
+import DentistService from "../../services/dentist";
+import { useSelector } from "react-redux";
 
 const DangKiLichRanh = () => {
+  const [lichhen, setLichHen] = useState([]);
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    DentistService.xemTableLichNS(user.MANS).then((res) => {
+      setLichHen(res || []);
+    });
+  }, []);
   return (
     <>
       <div className="">
         <TableLichHen data={lichhen4} />
-        
       </div>
     </>
   );
